e95f4c275932b95a7a49cf3a687a132648eeb6bc
Pham van Truong <30306380+phamtr@users.noreply.github.com>
2018-03-01 16:01:18 +0700

    update1/3/2018

--- api/routes/oders.js
+++ api/routes/oders.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const router = express.Router();
 
+// Handle incoming Get requests to /orsers
 router.get('/', (req, res, next) =>{
     res.status(200).json({
 message: 'Order were fetched'
@@ -8,19 +9,24 @@ message: 'Order were fetched'
 });
 
 router.post('/', (req, res, next) =>{
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-message: 'Order was created'
+message: 'Order was created',
+order: order
     });
 });
 
 router.get('/:orderId', (req, res, next) =>{
-    res.status(201).json({
+    res.status(200).json({
 message: 'Order details',
 orderId: req.params.orderId
     });
 });
 router.delete('/:orderId', (req, res, next) =>{
-    res.status(201).json({
+    res.status(200).json({
 message: 'Order deleted',
 orderId: req.params.orderId
     });
--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/databse');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/oders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) =>{
+    if(err){
+        console.log('Could Not connect to database', err);
+    }else{
+        //console.log(config.secret);
+        console.log('Connected to database: ' + config.db);
+    }
+});
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+
+
+// Routes which should handle requests
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) =>{
+const error = new Error('Not found');
+error.status = 404;
+next(error);
+});
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
