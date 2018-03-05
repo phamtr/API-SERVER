c44e72510b2e43227c41f6075eeda05e376ab5e3
Pham van Truong <30306380+phamtr@users.noreply.github.com>
2018-03-06 00:04:06 +0700

    UPdate

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,8 @@ const express =require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, './uploads/');
@@ -63,7 +65,7 @@ router.get('/', (req, res, next) =>{
 
 });
 
-router.post('/', upload.single('productImage'), (req, res, next) =>{
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) =>{
     console.log(req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
