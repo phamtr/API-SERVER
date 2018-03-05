e65d4e13ec26b5360ec9aa411becb0d2658bb884
Pham Van Truong <pvtruong115@gmail.com>
2018-03-05 16:36:39 +0700

    Update5/3/2018

--- config/databse.js
+++ config/databse.js
@@ -2,7 +2,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports=
 {
    // uri: 'mongodb://localhost:27017/mean-angular-2',
-    uri: 'mongodb://truong:truong@ds247178.mlab.com:47178/angular-2-app',
+    uri: 'mongodb://truong2:truong2@ds255958.mlab.com:55958/api-app',
     secret: crypto,
-    db: 'angular-2-app'
+    db: 'api-app'
 }
