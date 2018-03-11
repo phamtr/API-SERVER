288ed67df42c57187c59ccda94a80b4e16320d67
Pham van Truong <30306380+phamtr@users.noreply.github.com>
2018-03-11 23:45:56 +0700

    Update

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const mongoose = require('mongoose');
 
 exports.products_get_all = (req, res, next) =>{
     Product.find()
@@ -39,3 +40,113 @@ exports.products_get_all = (req, res, next) =>{
    }); 
    
    }
+
+   exports.products_create_product = (req, res, next) =>{
+    console.log(req.file);
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
+        productImage: req.file.path
+    }); 
+    product.save()
+            .then(result =>{
+                console.log(result);
+                res.status(201).json({message: 'Created product successfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    productImage: result.productImage,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + result._id
+                    }
+                }
+                });
+            })
+            .catch(err =>{
+                console.log(err);
+                res.status(500).json({error: err});
+            
+            });                
+           
+}
+
+exports.products_get_product = (req, res, next) =>{
+    const id = req.params.productId;
+    Product.findById(id)
+    .select('name price _id productImage')
+    .exec()
+    .then(doc =>{
+        console.log("From database", doc);
+        if(doc){
+            res.status(200).json({
+                product: doc,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/'
+                }
+            });
+        }else{
+            res.status(404).json({ message: 'No valid entry found for provided ID'});
+        }
+        
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({error: err});
+    
+    }); 
+    }
+
+    exports.products_delete_product = (req, res, next) =>{
+        const id = req.params.productId;
+        Product.remove({_id: id})
+        .exec()
+     .then(docs =>{
+    res.status(200).json({
+        message: 'Product deleted!',
+        request: {
+            type: 'POST',
+            url: 'http://localhost:3000/products',
+            body: { name: 'String', price: 'Number'}
+    
+        }
+    });
+     })
+     .catch(err =>{
+        console.log(err);
+        res.status(500).json({error: err});
+    
+     }); 
+    
+    }
+
+    exports.products_patch_product = (req, res, next) =>{
+        const id = req.params.productId;
+        const updateOps = {};
+        for (ops of req.body){
+            updateOps[ops.propName] = ops.value;
+        }
+        Product.update({_id: id}, { $set: updateOps })
+        .exec()
+        .then(result =>{
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+    
+            });
+    
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({error: err});
+        
+         }); 
+        
+    
+        }
