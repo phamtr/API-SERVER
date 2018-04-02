fdc78d81e60225fcdc5d09f23d8c6409e983bb6c
Pham van Truong <30306380+phamtr@users.noreply.github.com>
2018-04-02 22:50:55 +0700

    Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config/databse');
-
+const corspermission = require('./cors');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/oders');
 const userRoutes = require('./api/routes/user');
@@ -28,8 +28,9 @@ app.use(bodyParser.json());
 
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+   
 }));
+app.use(corspermission.permission);
 
 
 
