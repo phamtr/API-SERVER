07f0c83ac75f5f48416536bbe9b4f101b4ae02fd
Pham Van Truong <pvtruong115@gmail.com>
2018-04-13 10:47:31 +0700

    Update

--- api/models/product.js
+++ api/models/product.js
@@ -3,8 +3,16 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true},
+    description: {type: String},
     price: { type: Number, required: true},
-    productImage: {type: String, required: true }
+    productImage: {type: String, required: true },
+    show: { type: Boolean, default: true},
+    inStock: [{
+        createdBy: {type: String},
+        CreatedAt: {type: Date, default: Date.now()},
+        quantity: { type: Number, default: 1},
+        note: { type: String}
+    }]
 });
 
 module.exports = mongoose.model('Product', productSchema);
